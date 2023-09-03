9fa777b9954362362837b4946bc6b69bd0544a49
yang3666 <2293381927@qq.com>
2023-09-03 23:25:02 +0800

    spring容器的扩展点

---
+++ helloWorld-processor/src/main/java/learning/spring/helloworld/HelloBeanPostProcessor.java
@@ -0,0 +1,24 @@
+package learning.spring.helloworld;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class HelloBeanPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean,String beanName){
+        if ("hello".equals(beanName)) {
+            System.out.println("Hello postProcessBeforeInitialization");
+        }
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if ("hello".equals(beanName)) {
+            System.out.println("Hello postProcessAfterInitialization");
+        }
+        return bean;
+    }
+
+}
---
+++ helloWorld-processor/src/test/java/learning/spring/helloworld/helloworldprocessor/HelloWorldProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.spring.helloworld.helloworldprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
